8c4108a166e93f6176e79f0cda498ebf5fa84563
Stefan Saran <57372072+Stefan-Saran@users.noreply.github.com>
2020-05-30 23:23:43 +0200

    Update RE-LIC.py

--- RE-LIC.py
+++ RE-LIC.py
@@ -36,12 +36,15 @@ filename = date_format + str(".txt")
 
 
 def string_to_integer():
-    What_User_Wrote = lines_box.get()
-    Convert_To_Int = int(What_User_Wrote)
-    return Convert_To_Int
+    try:
+        What_User_Wrote = lines_box.get()
+        Convert_To_Int = int(What_User_Wrote)
+        return Convert_To_Int
+    except:
+        tk.messagebox.showerror("Error", "Only Numbers allowed!")
 
 
-def Generatung_lines2(popup):
+def Generatung_lines2():
     path_name = "Random lines generated"
     a = """"""
     c = 0
@@ -59,12 +62,11 @@ def Generatung_lines2(popup):
                 number2 = (number.replace("+", letter))
                 number3 = (number2.replace("!", letter2))
                 a = a + (str(number3))
-        popup.title("Writing lines...")
     completeName2.write('\n'.join(textwrap.wrap(a, d)))
     completeName2.close()
 
 
-def Generating_lines1(entry, popup):
+def Generating_lines1(entry, lines_counter):
     path_name = "Random lines generated"
     a = """"""
     c = 0
@@ -73,7 +75,7 @@ def Generating_lines1(entry, popup):
     f = 0
     if not os.path.exists(path_name):
         os.makedirs(path_name)
-        Generatung_lines2(popup)
+        Generatung_lines2()
     else:
         completeName2 = os.path.join(path_name, filename+".txt")
         completeName2 = open(completeName2, "w")
@@ -86,7 +88,6 @@ def Generating_lines1(entry, popup):
                     number2 = (number.replace("+", letter))
                     number3 = (number2.replace("!", letter2))
                     a = a + (str(number3))
-            popup.title("Writing lines...")
         completeName2.write('\n'.join(textwrap.wrap(a, d)))
         completeName2.close()
 
@@ -96,10 +97,12 @@ def Generating_lines1(entry, popup):
         os.path.basename(__file__),
     )
     try:
+        lines_counter.withdraw()
         createFolder()
         Lines_Generator.quit()
         subprocess.Popen(command)
     except Exception:
+        lines_counter.deiconify()
         traceback.print_exc()
         sys.exit('fatal error occurred rerunning script')
     else:
@@ -136,11 +139,13 @@ def Lines_generating_time():
     lines_counter.wm_overrideredirect(True)
     lines_counter.config(menu="")
     lines_counter.title("Generating...")
-    tk.Label(lines_counter, text="Generating, please wait it depends on number of lines...")
+    tk.Label(lines_counter,
+             text="Generating, please wait it depends on number of lines...")
 
     progress = 0
     progress_var = tk.DoubleVar()
-    progress_bar = ttk.Progressbar(lines_counter, variable=progress_var, maximum=100)
+    progress_bar = ttk.Progressbar(
+        lines_counter, variable=progress_var, maximum=100)
     progress_bar.place(rely=0.25, relx=0.05, relwidth=0.9)
     lines_counter.pack_slaves()
 
@@ -184,8 +189,8 @@ def warning():
         messagebox.showerror("Error", "Line Box cannot be blank!")
     elif pattern_box.get() == c and b >= 0 or b >= 0 and a == 0:
         messagebox.showerror("Error", "Pattern Box cannot be blank!")
-    elif string_to_integer() > 50000:
-        messagebox.showinfo("Error", "The maximum number of lines is 50.000!")
+    elif string_to_integer() > 10000:
+        messagebox.showinfo("Error", "The maximum number of lines is 10.000!")
     else:
         disabling_and_warning()
 
@@ -219,7 +224,7 @@ def Help_window():
     """
     help_info += '\n'
     width = Label(help, text=help_info, width=120, height=10,
-              background="#1A1A1A", foreground="white", font=large_font2)
+                  background="#1A1A1A", foreground="white", font=large_font2)
     width.pack()
 
     help.transient(Lines_Generator)
@@ -239,8 +244,8 @@ def About_window():
     About = Toplevel(background="#1A1A1A")
     help_font = ('Verdana', 13)
     About.title("About")
-    width = 300     # popup window width
-    height = 400     # popup window height
+    width = 300
+    height = 400
     screenwidth = About.winfo_screenwidth()
     screenheight = About.winfo_screenheight()
     x = (screenwidth - width)/2
@@ -260,7 +265,7 @@ Creation date:
 """
     about_info += '\n'
     width = Label(About, text=about_info, width=120, height=18,
-              background="#1A1A1A", foreground="white", font=help_font)
+                  background="#1A1A1A", foreground="white", font=help_font)
     width.pack()
 
     release_date2 = tk.Label(width, text=release_date,
@@ -281,16 +286,28 @@ Creation date:
 def clearn_code_pattern(event):
     if pattern_box.get() == "Enter your code pattern here":
         pattern_box.delete(0, END)
+    else:
+        pass
 
 
 def clear_lines_box(event2):
     if lines_box.get() == "How many lines?":
         lines_box.delete(0, END)
+    else:
+        pass
 
 
-def insert_entry1_text(asd1):
-    if len(pattern_box.get()) == 0:
+def text_effect(asd1):
+    if len(pattern_box.get()) == 0 and len(lines_box.get()) == 0:
         pattern_box.insert(0, "Enter your code pattern here")
+        lines_box.insert(0, "How many lines?")
+        Lines_Generator.focus()
+    elif len(pattern_box.get()) == 0:
+        pattern_box.insert(0, "Enter your code pattern here")
+        Lines_Generator.focus()
+    elif len(lines_box.get()) == 0:
+        lines_box.insert(0, "How many lines?")
+        Lines_Generator.focus()
     else:
         pass
 
@@ -300,50 +317,65 @@ def insert_entry2_text(asd2):
         lines_box.insert(0, "How many lines?")
     elif lines_box.get() == "How many lines?":
         lines_box.delete(0, END)
+    else:
+        pass
+
+
+pattern_box_font = ('Verdana', 17)
+generate_button_font = ('Arial', 12)
+help_button_font = ("Arial", 10)
+about_button_font = ("Arial", 10)
 
 
 Canvas = tk.Canvas(height=600, width=1000, bg="#343434")
-Canvas.pack(fill="both", expand=True)
 
 Frame = tk.Frame(Canvas, height=600, width=1000, bg="#1A1A1A")
-Frame.pack(fill="both", expand=True)
 
-large_font = ('Verdana', 17)
 pattern_box = tk.Entry(Frame, state=NORMAL, bg="#F7F5EB",
-                 font=large_font, width=25)
-pattern_box.place(relx=0.5, rely=0.3, anchor="center")
-entry1_text = "Enter your code pattern here"
-pattern_box.insert(0, entry1_text)
-pattern_box.configure(foreground="gray")
-pattern_box.bind("<Button-1>", clearn_code_pattern)
+                       font=pattern_box_font, width=25)
+
 
 lines_box = tk.Entry(Frame, bg="#F7F5EB", width=22, font=("Verdana", 11))
-lines_box.place(relx=0.5, rely=0.5, anchor="center")
-b = "How many lines?"
-lines_box.insert(0, b)
-lines_box.configure(foreground="gray")
-lines_box.bind("<Button-1>", clear_lines_box)
 
-generate_button_font = ('Arial', 12)
 Generate_button = tk.Button(
     Frame, bg="#f0f0f0", font=generate_button_font, foreground="#525252", command=lambda: warning())
-Generate_button.place(rely=0.7, relx=0.35, relwidth=0.31, relheight=0.12)
-Generate_button.config(text="Generate")
 
-help_button_font = ("Arial", 10)
+
 help_button = tk.Button(Frame, bg="#F7F5EB", font=help_button_font,
                         foreground="#525252", command=Help_window)
-help_button.place(rely=0.03, relx=0.88, relwidth=0.1, relheight=0.075)
-help_button.config(text="Help")
 
-about_button_font = ("Arial", 10)
+
 about_button = tk.Button(Frame, bg="#F7F5EB", font=help_button_font,
                          foreground="#525252", command=About_window)
+
+entry1_text = "Enter your code pattern here"
+lines_box_text = "How many lines?"
+lines_box.insert(0, lines_box_text)
+pattern_box.insert(0, entry1_text)
+
+Canvas.pack(fill="both", expand=True)
+Frame.pack(fill="both", expand=True)
+pattern_box.place(relx=0.5, rely=0.3, anchor="center")
+lines_box.place(relx=0.5, rely=0.5, anchor="center")
+Generate_button.place(rely=0.7, relx=0.35, relwidth=0.31, relheight=0.12)
+help_button.place(rely=0.03, relx=0.88, relwidth=0.1, relheight=0.075)
 about_button.place(rely=0.03, relx=0.02, relwidth=0.1, relheight=0.075)
+
+pattern_box.configure(foreground="gray")
+lines_box.configure(foreground="gray")
+Generate_button.config(text="Generate")
+help_button.config(text="Help")
 about_button.config(text="About")
 
 center_window(600, 350)
 Lines_Generator.minsize(390, 200)
 Lines_Generator.geometry("500x300")
+
+help_button.bind("<Button-1>", text_effect)
+Generate_button.bind("<Button-1>", text_effect)
+about_button.bind("<Button-1>", text_effect)
+Frame.bind("<Button-1>", text_effect)
+pattern_box.bind("<Button-1>", clearn_code_pattern)
+lines_box.bind("<Button-1>", clear_lines_box)
 
 Lines_Generator.mainloop()
